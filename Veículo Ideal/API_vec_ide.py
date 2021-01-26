a513cfc8dac10fc1e9d48f823844b7b5c205a7f0
ian011403 <74920083+ian011403@users.noreply.github.com>
2021-01-25 22:39:26 -0300

    Update API_vec_ide.py

--- Veículo Ideal/API_vec_ide.py
+++ Veículo Ideal/API_vec_ide.py
@@ -1,22 +1,20 @@
 # Esse script é reponsavel por criar um servidor com uma API local. 
-# Aqui escrevo as funcionalidades da API usando o web framework Flask.
 # A API gerada por esse script resolve o problema do veiculo ideal
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from veiculo_ideal import vec_ide
 
 app = Flask("Acha Veículo")
-app.config["DEBUG"] = True
 
 @app.route('/veiculoideal', methods=['POST'])
 def veiculo_ideal():
 
-    itens = request.get_json() 
+    itens = request.get_json() # Converte os dados recebido em fomrato JSON para um dicionário 
 
-    resposta = vec_ide(itens)
+    resposta = vec_ide(itens) # Devolve um dicionário na forma { 'Lala': 'Veiculo', Ogi: 'Veiculo' }
 
-    resposta_json  = jsonify(resposta)
+    resposta_json  = jsonify(resposta) # Gera resposta em forma JSON
 
-    return resposta_json  
+    return resposta_json  # Retorna resposta para o usuário
 
 app.run()
