5c6f5750f541fb53289de4289f4363e270edeff2
ian011403 <74920083+ian011403@users.noreply.github.com>
2021-01-25 21:50:17 -0300

    Update script_fibo.py

--- Fibonacci/script_fibo.py
+++ Fibonacci/script_fibo.py
@@ -1,27 +1,10 @@
-# O objetivo dessa API é receber um numero inteiro n do usuário e
-# processar o n-ésimo termo da sequência de Fibonacci chamando a função
-# ótima Fibonacci do modulo fibo
-
-# Essa API pode recerber um arquivo .txt com multiplas entradas, retornando 
-# para cada entrada o devido termo da sequencia de FIbonacci. A respostas serão
-# escritas na mesmo ordem com que aparecem as entradas no arq. txt. 
-# 
-# A sintaxe desse arquivo deve ser de uma entrada a cada linha, para facilitar a 
-# visualização das respsotas posteriormente.   
+# O objetivo dessa scrip é testar a função fibo, do modulo Fibonacci que retorna o n-ésimo 
+# termo da sequência de Fibonacci. Esse scrip foi feito para ser usado no terminal para testes rápidos.
 
 from fibo import Fibonacci
 
-resposta = input("Deseja usar arquivo com multiplas entradas? (Digite s ou n)")
+n = int(input("Digite qual termo da sequência de Fibonacci você quer calcular:" ))
 
-if resposta == s:
+F_n = Fibonacci(n)
 
-    file = input("Escreva o caminho até o arquivo a partir da root ou do diretorio atual")
-
-
-if resposta == n
-
-    n = int(input("Digite qual termo da sequência de Fibonacci você quer calcular:" ))
-
-    F_n = Fibonacci(n)
-
-    print("O termo " + str(n) + "-ésimo da sequência é " + str(F_n))
+print("O termo " + str(n) + "-ésimo da sequência é " + str(F_n))
