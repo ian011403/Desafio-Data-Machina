b5b12b0aa4d495e68287df456139d2b679cde02e
ian011403 <74920083+ian011403@users.noreply.github.com>
2021-01-25 23:08:39 -0300

    Update API_vec_ide.py

--- Veículo Ideal/API_vec_ide.py
+++ Veículo Ideal/API_vec_ide.py
@@ -1,5 +1,11 @@
 # Esse script é reponsavel por criar um servidor com uma API local. 
-# A API gerada por esse script resolve o problema do veiculo ideal
+# A API gerada por esse script resolve o problema do veiculo ideal.
+
+# As requests devem ser feitas com método post cujo o corpo possue a formatação JSON senguindo o padrão:
+#[
+#    { "item": "café", "largura":35, "altura":20, "espessura": 30, "peso":1 },
+#    { "item": "leite", "largura":35, "altura":20, "espessura": 30, "peso":1 }
+#]
 
 from flask import Flask, request, jsonify
 from veiculo_ideal import vec_ide
